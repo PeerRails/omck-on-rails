fe9a457004fd3a166e570626f65884a237496dae
peer <peer@peer-VirtualBox.(none)>
2014-09-13 14:40:43 +0400

    new puma config, new path PUMAPATH for puma and HOMEPATH for repo

--- puma.rb
+++ puma.rb
@@ -1,7 +1,7 @@
-directory '/home/prails/git/omck-on-rails'
-environment 'production'
+directory ENV["PUMAPATH"]
+environment ENV["RAILS_ENV"] || 'development'
 daemonize
-pidfile '/home/prails/git/omck-on-rails/tmp/pids/puma.pid'
-state_path '/home/prails/git/omck-on-rails/tmp/pids/puma.state'
-stdout_redirect '/home/prails/git/omck-on-rails/log/stdout.log', '/home/prails/git/omck-on-rails/log/stderr.log', true
+pidfile ENV["PUMAPATH"]+'/tmp/pids/puma.pid'
+state_path ENV["PUMAPATH"]+'/tmp/pids/puma.state'
+stdout_redirect ENV["PUMAPATH"]+'/log/stdout.log', ENV["PUMAPATH"]+'/log/stderr.log', true
 bind 'tcp://0.0.0.0:3000'
