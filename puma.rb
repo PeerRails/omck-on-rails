c57f78c947cae8a53f53330815936586c86a63f5
Peer Rails <prails@omck.ws>
2014-09-13 04:15:25 -0400

    delete logs

--- puma.rb
+++ puma.rb
@@ -3,5 +3,5 @@ environment 'production'
 daemonize
 pidfile '/home/prails/git/omck-on-rails/tmp/pids/puma.pid'
 state_path '/home/prails/git/omck-on-rails/tmp/pids/puma.state'
-stdout_redirect '/home/prails/git/omck-on-rails/log/stdout', '/home/prails/git/omck-on-rails/log/stderr', true
+stdout_redirect '/home/prails/git/omck-on-rails/log/stdout.log', '/home/prails/git/omck-on-rails/log/stderr.log', true
 bind 'tcp://0.0.0.0:3000'
