85ca688b1fcf261420be1c64a3825c6f180afee6
Peer Rails <peer@omck.ws>
2014-11-14 16:11:32 +0400

    edit puma config file to add env port

--- puma.rb
+++ puma.rb
@@ -1,7 +1,7 @@
 directory ENV["PUMAPATH"]
 environment ENV["RAILS_ENV"] || 'development'
-daemonize
 pidfile ENV["PUMAPATH"]+'/pids/puma.pid'
 state_path ENV["PUMAPATH"]+'/pids/puma.state'
-stdout_redirect ENV["PUMAPATH"]+'/log/stdout.log', ENV["PUMAPATH"]+'/log/stderr.log', true
-bind 'tcp://0.0.0.0:3000'
+stdout_redirect nil, ENV["PUMAPATH"]+'/log/stderr.log', true
+bind "tcp://0.0.0.0:#{ENV["PUMAPORT"] || 3000}"
+daemonize
