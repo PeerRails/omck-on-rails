39bfe50cffb5f71de86d4911691519c6cbeed41e
Peer Rails <peer@omck.ws>
2015-04-30 17:39:08 +0300

    SourceTree after cleaning temp started working normally

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,7 +2,6 @@ class Video < ActiveRecord::Base
   belongs_to :key
   belongs_to :user
   validates :uid, presence: true, uniqueness: true
-  validates :youtube_id, uniqueness: true
 
   scope :deleted, -> { where(deleted: true) }
 
