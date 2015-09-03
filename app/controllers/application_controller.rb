6818a5f5a9291e0744473a2521099532761d41b7
Peer Rails <peer@omck.ws>
2015-09-03 13:59:02 +0300

    =\

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+
   protect_from_forgery with: :exception
 
 	def error code
@@ -45,5 +44,4 @@ class ApplicationController < ActionController::Base
     end
 
     helper_method :current_user
-    
 end
