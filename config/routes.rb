a2d042956f35ef3383e18f6df4ad6ca21f109ee7
Peer Rails <peer@omck.ws>
2015-11-27 18:36:38 +0300

    preset route for new api

--- config/routes.rb
+++ config/routes.rb
@@ -1,117 +1,107 @@
 Rails.application.routes.draw do
 
-  #Auth
-  match '/auth/:provider/callback' => 'users#login', via: [:get, :post]
-  get 'auth_session' => 'application#session_auth'
-  get 'logout' => 'users#logout'
-  post 'home/manage_perm' => 'users#manage_perm'
-  post 'home/remove_streamer' => 'users#remove_streamer'
-
-  #Old Home
-  get 'home' => 'staff#index'
-
-  #API
-  get 'channel/move_record' => 'nginx#move_record'
-  get 'channel/live' => 'streams#get_live'
-  get 'channel/all' => 'streams#get_all'
-  get 'channel/:service/:channel' => 'streams#get_channel'
-
-  #bitdash
-  get 'bitdash/:channel' => 'channels#bitdash'
-  get 'bitdash' => 'channels#bitdash'
-
-  #NGINX Controller
-  get 'auth_stream' => 'nginx#get_key'
-  post 'auth_stream' => 'nginx#get_key'
-  get 'incr_stream' => 'nginx#increase_viewer_count'
-  get 'decr_stream' => 'nginx#decrease_viewer_count'
-  get 'end_cinema' => 'nginx#end_cinema'
-
-  #Keys
-  get 'home/remake_key' => 'keys#remake_key'
-  post 'home/make_key' => 'keys#make_key'
-  post 'home/change_key' => 'keys#change_key'
-  post 'home/expire_key' => 'keys#expire_key'
-
-  #Pages
-  get 'faq' => 'home#faq'
-  get 'faq-irc' => 'home#faq_irc'
-  get 'guest' => 'home#guest'
-  post 'guest' => 'home#guest'
-
-  #Channel CRUD
-  post 'home/channel/new' => 'channels#new'
-  post 'home/channel/edit' => 'channels#edit'
-  post 'home/channel/delete' => 'channels#delete'
-
-  #Tweet Controller
-  post 'home/tweet' => 'tweets#tweet'
-
-  #Video
-  get 'user/vids' => 'videos#list'
-  get 'user/vids/check' => 'videos#check_deleted'
-  delete 'user/vids/delete' => 'videos#delete'
-
   #Root
   root 'home#index'
 
-  #resources :keys
-  #resources :tweets
+  #auth
+  match '/auth/:provider/callback' => 'users#login', via: [:get, :post]
+  get 'auth_session' => 'application#session_auth'
+  get 'logout' => 'users#logout'
 
-  #avconv -re -i /home/prails/Downloads/stream.flv -f flv rtmp://localhost:1935/live/omckws?key=50adf9fc-8499-4d45-b499-de831586cd9a
+  #Channel API
+  get 'channel/live' => 'channels#list_live', defaults: {page: 0}
+  get 'channel/all' => 'channels#index', defaults: {page: 0}
+  get 'channel/:service/:channel' => 'channels#show'
+  get 'service/:service' => 'channels#list_channels', defaults: {service: "hd"}
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  post 'channel/new' => 'channels#new'
+  post 'channel/:service/:channel/update'
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  #User API
+  get 'user/:twitter_id' => 'users#show'
+  get 'user/:twitter_id/videos' => 'users#videos'
+  post 'user/:twitter_id/update' => 'users#update'
+  post 'user/:twitter_id/grant' => 'users#grant'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  match 'user/guest/auth' => 'users#guest_in', via: [:get, :post]
+  get 'user/guest/videos' => 'user#guest_video'
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  #Key API
+  get 'auth_stream' => 'keys#auth'
+  post 'keys/create' => 'keys#create'
+  post 'keys/expire' => 'keys#expire'
+  post 'keys/update' => 'keys#update'
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
 
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
+  #Video API
+  get 'video' => 'videos#index'
+  post 'video/:id/save' => 'videos#save'
+  delete 'video' => 'videos#remove'
 
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
+  #Pages:
 
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
+  #bitdash
+  get 'bitdash/:channel' => 'channels#bitdash', defaults: {channel: "records"}
+  get 'bitdash' => 'channels#bitdash'
 
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
+  #home
+  get '/home' => 'home#cabinet'
+  get 'faq' => 'home#faq'
+  get 'home/admin' => 'home#admin'
 
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+#avconv -re -i /home/prails/Downloads/stream.flv -f flv rtmp://localhost:1935/live/omckws?key=50adf9fc-8499-4d45-b499-de831586cd9a
+=begin
+    #Auth
+    match '/auth/:provider/callback' => 'users#login', via: [:get, :post]
+    get 'auth_session' => 'application#session_auth'
+    get 'logout' => 'users#logout'
+    #post 'home/manage_perm' => 'users#manage_perm'
+    #post 'home/remove_streamer' => 'users#remove_streamer'
+
+    #Old Home
+    #get 'home' => 'staff#index'
+
+    #API
+    get 'channel/move_record' => 'nginx#move_record'
+    get 'channel/live' => 'streams#get_live'
+    get 'channel/all' => 'streams#get_all'
+    get 'channel/:service/:channel' => 'streams#get_channel'
+
+    #bitdash
+    get 'bitdash/:channel' => 'channels#bitdash'
+    get 'bitdash' => 'channels#bitdash'
+
+    #NGINX Controller
+    get 'auth_stream' => 'nginx#get_key'
+    post 'auth_stream' => 'nginx#get_key'
+    get 'incr_stream' => 'nginx#increase_viewer_count'
+    get 'decr_stream' => 'nginx#decrease_viewer_count'
+    get 'end_cinema' => 'nginx#end_cinema'
+
+    #Keys
+    get 'home/remake_key' => 'keys#remake_key'
+    post 'home/make_key' => 'keys#make_key'
+    post 'home/change_key' => 'keys#change_key'
+    post 'home/expire_key' => 'keys#expire_key'
+
+    #Pages
+    get 'faq' => 'home#faq'
+    get 'faq-irc' => 'home#faq_irc'
+    get 'guest' => 'home#guest'
+    post 'guest' => 'home#guest'
+
+    #Channel CRUD
+    post 'home/channel/new' => 'channels#new'
+    post 'home/channel/edit' => 'channels#edit'
+    post 'home/channel/delete' => 'channels#delete'
+
+    #Tweet Controller
+    post 'home/tweet' => 'tweets#tweet'
+
+    #Video
+    get 'user/vids' => 'videos#list'
+    get 'user/vids/check' => 'videos#check_deleted'
+    delete 'user/vids/delete' => 'videos#delete'
+=end
+
 end
