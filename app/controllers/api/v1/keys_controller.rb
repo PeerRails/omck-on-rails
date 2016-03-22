b6523df4ee5fa633f6d9ebd0e47f54547dbeb7d9
Peer Rails <peer@omck.ws>
2016-03-22 18:28:13 +0300

    add create key action

--- app/controllers/api/v1/keys_controller.rb
+++ app/controllers/api/v1/keys_controller.rb
@@ -16,6 +16,15 @@ module Api
         keys = Key.is_guest
         render json: keys
       end
+
+      def create
+        key = Key.new(key_params)
+        if key.save
+          render json: key
+        else
+          render json {error: true, message: key.errors}
+        end
+      end
     end
   end
 end
