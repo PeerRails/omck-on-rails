02eea0d706fd9e57c39bdd52f5a60db0b2d74a8d
Peer Rails <peer@omck.ws>
2016-09-28 13:29:06 +0300

    rm helpers, add username, channel and service attr to StreamSerializer

--- app/serializers/stream_serializer.rb
+++ app/serializers/stream_serializer.rb
@@ -27,6 +27,37 @@
 #    }
 
 class StreamSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :key_id, :channel_id, :game, :created_at, :ended_at
+  attributes :id, :user_id, :key_id, :channel_id, :game, :created_at, :ended_at, :channel, :service, :username
+
+  # Return Channel name
+  # @return [String]
+  def channel
+      channel_obj.channel
+  end
+
+  # Return Channel service
+  # @return [String]
+  def service
+      channel_obj.service
+  end
+
+  # Return User name
+  # @return [String]
+  def username
+      user_obj.name
+  end
+
+  private
+  # Return channel object by channel id
+  # @return [Channel]
+  def channel_obj
+      Channel.select(:channel, :service).find(object.channel_id) || Channel.new(service: "Not Found", channel: "Not Found")
+  end
+
+  # Return user object by user id
+  # @return [Channel]
+  def user_obj
+      User.select(:name).find(object.user_id) || User.new(name: "Not Found")
+  end
 
 end
