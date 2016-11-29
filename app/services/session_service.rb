ecadcfc17c397398a87e8f66ee894f87b2c580e4
Peer Rails <peer@omck.ws>
2016-11-29 17:54:48 +0300

    add session service and specs

---
+++ app/services/session_service.rb
@@ -0,0 +1,29 @@
+class SessionService
+    # Database stored sessions
+    def initialize(client)
+        @client = client || Client.new
+    end
+
+    # Add session attached to client
+    # @params session_id [String] String with encrypted unique session identificator
+    # @return [Boolean] true if saved or false on reject
+    def attach_to_client(session_id)
+	Session.create(:client_id => @client.id,
+              :session_id => session_id,
+              :expires => @client.remember_at
+              )
+    end
+
+    # Detach session string id from client (logout)
+    # @params session_id [String] String with encrypted unique session identificator
+    # @return [Boolean] true if saved or false on reject
+    def destroy(session_id)
+        return false if session_id.nil? || session_id.empty?
+	session = Session.where(:session_id => session_id, client_id: @client.id).first
+	unless session.nil?
+            session.update(:expires => DateTime.now)
+        else
+            true
+        end
+    end
+end
