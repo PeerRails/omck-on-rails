2bd4717f3faa7bdc60a0f93b4ddc2598b3784fda
Peer Rails <peer@omck.ws>
2016-12-19 12:42:12 +0300

    start refactoring

--- spec/controllers/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/omniauth_callbacks_controller_spec.rb
@@ -1,18 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe OmniauthCallbacksController, type: :controller do
-    describe "authorization through other services" do
-        it "should login through twitter" do
-            client = create(:client, :streamer)
-            create(:account, client_id: client.id)
-            request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:twitter]
-            post :twitter, session: {session_id: Faker::Internet.password }
-            expect(response).to redirect_to home_path
-        end
-        it "should create new client" do
-            request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:twitter]
-            expect{ post :twitter, session: {session_id: Faker::Internet.password } }.to change{Client.count}.by(1)
-        end
-    end
 
 end
